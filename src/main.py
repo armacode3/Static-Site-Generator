80fbd5dc279e9151ba9113236193462f63c6c96b
armacode3 <zavaarmando0@gmail.com>
2025-11-15 21:11:31 -0500

    Start unittest

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode, TextType
+
+
+if __name__ == '__main__':
+    # plain text
+    print(TextNode("Hello world", TextType.TEXT))
+
+    # link
+    print(TextNode("Boot.dev", TextType.LINK, "https://www.boot.dev"))
+
+    # bold
+    print(TextNode("Be bold", TextType.BOLD))

a354067b9dd6bbfcd761968a9b7ab56401c383b9
armacode3 <zavaarmando0@gmail.com>
2025-11-19 17:48:51 -0500

    Update for github pages

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import os
 import shutil
+import sys
 from copystatic import copy_directory_recursively
 from page_generation import generate_pages_recursive
 
 if __name__ == '__main__':
-    if os.path.exists("public/"):
-        shutil.rmtree("public/")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    
+    print(f"Generating site with basepath: {basepath}")
 
-    print("Copying static files...")
-    copy_directory_recursively("static/", "public/")
+    dest_dir = "docs"
 
-    print("Generating content...")
-    generate_pages_recursive("content/", "template.html", "public/")
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+
+    copy_directory_recursively("static/", dest_dir)
+    generate_pages_recursive("content/", "template.html", dest_dir, basepath)
--- src/page_generation.py
+++ src/page_generation.py
@@ -14,7 +14,7 @@ def extract_title(markdown):
     raise Exception("No h1 header")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as f:
@@ -29,6 +29,8 @@ def generate_page(from_path, template_path, dest_path):
 
     full_html = (html_contents.replace("{{ Title }}", title).replace("{{ Content }}", mark_to_html))
 
+    full_html = full_html.replace('href="/', f'href="{basepath}')
+    full_html = full_html.replace('src="/', f'src="{basepath}')
 
     dirpath = os.path.dirname(dest_path)
     
@@ -39,7 +41,7 @@ def generate_page(from_path, template_path, dest_path):
         f.write(full_html)
 
         
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
@@ -47,7 +49,6 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(from_path):
             if from_path.endswith(".md"):
                 dest_path = Path(dest_path).with_suffix(".html")
-                generate_page(from_path, template_path, dest_path)
-        
+                generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
